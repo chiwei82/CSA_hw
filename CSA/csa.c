b61b2987a7763f5364d0b0790f4122bf39ccb6af
Tony <s481597@gmail.com>
2025-12-04 10:41:36 +0000

    add test for functions

--- CSA/csa.c
+++ CSA/csa.c
@@ -11,25 +11,24 @@ csa* csa_init(void)
 
 bool csa_get(csa* c, int idx, int* val)
 {
-   // If any of the pointers is NULL, 
-   // or if the cell is unset, returns false.
+   // If any of the pointers is NULL, return false
    if (c == NULL || val == NULL) {
       return false;
    }
    // 1. check if block exists
-   int bit_index = idx % MSKLEN;
-   int bp_index = findBlock(c, idx);
-   if (bp_index == BLOCK_NOT_FOUND) {
+   int bp_idx = findBlock(c, idx);
+   if (bp_idx == BLOCK_NOT_FOUND) {
       return false;
    }
-   block *bp = &c->b[bp_index];
+   block *bp = &c->b[bp_idx];
+
    // 2. check if value exists
-   bool isExist = bp->msk & ((mask_t)1 << bit_index);
-   if (!isExist) {
+   int bit_idx = idx % MSKLEN;
+   if (!isValExist(bp->msk, bit_idx)) {
       return false;
    }
-   // 3. get value
-   int pos = findPosition(bp, bit_index);
+   // 3. get value and copied into val
+   int pos = findPosition(bp, bit_idx);
    *val = bp->vals[pos];
    return true;
 }
@@ -40,8 +39,8 @@ bool csa_set(csa* c, int idx, int val)
       return false;
    } //
    // 1. if not block exists do block increment
-   int bp_index = findBlock(c, idx);
-   if (bp_index == BLOCK_NOT_FOUND) {
+   int bp_idx = findBlock(c, idx);
+   if (bp_idx == BLOCK_NOT_FOUND) {
       incrementBlock(c, idx);
    }
    // 2. set value
@@ -73,32 +72,7 @@ void csa_tostring(csa* c, char* s)
    strcat(s, " ");
 
    // concat data part
-   for (int i = 0; i < c->n; i++) {
-      
-      // close '{'
-      strcat(s, "{");
-   
-      int count = findPosition(&c->b[i], MSKLEN);
-      snprintf(tmp, BIGSTR , "%d|", count);
-      strcat(s, tmp);
-      
-      int cur_val_index = 0;
-      for (int bit_index = 0; bit_index < MSKLEN; bit_index++) {
-         if (c->b[i].msk & ((mask_t)1 << bit_index)) {
-            int full_index = c->b[i].offset + bit_index;
-            int ram_val = c->b[i].vals[cur_val_index];
-            snprintf(tmp, BIGSTR, "[%d]=%d", full_index, ram_val);
-            strcat(s, tmp);
-            if (cur_val_index != count - 1){
-               strcat(s, ":");
-            }
-            cur_val_index++;
-         }
-      }
-
-      // close '}'
-      strcat(s, "}");
-   }
+   concat_data(c, s);
 }
 
 void csa_free(csa** l)
@@ -120,26 +94,71 @@ void test(void)
    char str[BIGSTR];
    int n = 1;
 
+   // csa_init
    csa_tostring(c, str);
    assert(strcmp(str, "")==0);
-   // init
+   
    c = csa_init();
-   assert(!csa_get(c, 0, &n));
-   assert(!csa_get(c, 2, &n));
-   // setting test
-   assert(csa_set(c, 2,  25));
-   // Add csa[3]=30
-   assert(csa_set(c, 3,  30));
-   // Add csa[63]=100
-   assert(csa_set(c, 100, 100));
-   // Getters
-   assert(csa_get(c,  2, &n));
-   assert(n==25);
-   assert(csa_get(c,  3, &n));
-   assert(n==30);
-   assert(csa_get(c, 100, &n));
-   assert(n==100);
    csa_tostring(c, str);
+   assert(strcmp(str, "0 blocks")==0);
+   // csa_set
+   assert(csa_set(c, 2,  25));
+   assert(csa_set(c, 3,  30));
+   // csa_get
+   assert(!csa_get(c, 0, &n));
+   assert(csa_get(c,  2, &n));
+   // csa_tostring
+   csa_tostring(c, str);
+   assert(strcmp(str, "1 block {2|[2]=25:[3]=30}")==0);
+
+   // findBlock
+   assert(findBlock(c, 2) == 0);
+   assert(findBlock(c, 64) == BLOCK_NOT_FOUND);
+   assert(findBlock(c, 256) == BLOCK_NOT_FOUND);
+   // findPosition
+   assert(findPosition(&c->b[0], 0) == 0);
+   assert(findPosition(&c->b[0], 3) == 1);
+   assert(findPosition(&c->b[0], 5) == 2);
+   // isValExist
+   assert(!isValExist(c->b[0].msk, 63));
+   assert(isValExist(c->b[0].msk, 2));
+   assert(isValExist(c->b[0].msk, 3));
+   // void incrementBlock(inside csa_set)
+   // so we test if incrementBlock works as expected 
+   // by testing after csa_set possibly call incrementBlock
+   assert(csa_set(c, 67,  10));
+   assert(findBlock(c, 67) == 1);
+   assert(csa_set(c, 257,  10));
+   assert(findBlock(c, 257) == 2);
+   // void update_block(inside csa_set)
+   // if we can retrieve the previously set value
+   // it means update_block works
+   assert(csa_get(c,  67, &n));
+   assert(csa_get(c,  257, &n));
+   // move_and_insert
+   // desired: if we insert block with lower offset
+   // it will still stay ordered by offset
+   c = csa_init();
+   assert(csa_set(c, 257,  25));
+   assert(c->b[0].offset == 256);
+   assert(csa_set(c, 66,  30));
+   assert(c->b[0].offset == 64);
+   assert(c->b[1].offset == 256);
+   assert(csa_set(c, 2,  10));
+   assert(c->b[0].offset == 0);
+   assert(c->b[1].offset == 64);
+   assert(c->b[2].offset == 256);
+   // concat_data
+   str[0] = '\0'; // reset str
+   concat_data(c, str);
+   assert(strcmp(str, "{1|[2]=10}{1|[66]=30}{1|[257]=25}")==0);
+
+   #ifdef EXT
+   // csa_foreach
+   // csa_delete
+   // delete
+   // additional_free
+   #endif
 
    // clean up: for LeakSanitizer
    csa_free(&c);
@@ -165,20 +184,17 @@ bool csa_delete(csa* c, int idx)
       return false;
    }
    // find which block
-   int bp_index = findBlock(c, idx);
-   if (bp_index == BLOCK_NOT_FOUND) {
+   int bp_idx = findBlock(c, idx);
+   if (bp_idx == BLOCK_NOT_FOUND) {
       return false;
    }
+   block *bp = &c->b[bp_idx];
    
-   block *bp = &c->b[bp_index];
    // find which pos inside block
-   int bit_index = idx % MSKLEN;
-   int pos = findPosition(bp, bit_index);
+   int bit_idx = idx % MSKLEN;
+   int pos = findPosition(bp, bit_idx);
    // delete
-   // bp is pointed to desired block
-   // bit_index is the index to unset inside block msk << ?
-   // pos is the index to unset inside block val[pos]
-   bool succ = delete(c, bp_index, bit_index, pos);
+   bool succ = delete(c, bp_idx, bit_idx, pos);
    if (succ) {
       return true;
    }
@@ -208,16 +224,15 @@ void incrementBlock(csa* c, int idx)
 void update_block(csa* c, int idx, int val)
 {
    // indexes
-   int bit_index = idx % MSKLEN;
+   int bit_idx = idx % MSKLEN;
    // update block
-   int bp_index = findBlock(c, idx);
-   block *bp = &c->b[bp_index];
+   int bp_idx = findBlock(c, idx);
+   block *bp = &c->b[bp_idx];
    // 1. find size and position
    int counts = findPosition(bp, MSKLEN); // total occpuied
-   int pos = findPosition(bp, bit_index); // insert position
+   int pos = findPosition(bp, bit_idx);   // insert position
    // 2. realloc and move values if needed
-   bool val_existed = bp->msk & ((mask_t)1 << bit_index);
-   if (!val_existed) {
+   if (!isValExist(bp->msk, bit_idx)) {
       int new_len = counts + 1;
       bp->vals = realloc(bp->vals, sizeof(int) * (new_len));
       // linear move one index to the right
@@ -228,13 +243,13 @@ void update_block(csa* c, int idx, int val)
    // 3. update vals(array)
    bp->vals[pos] = val;
    // 4. upadte mask
-   bp->msk |= ((mask_t)1 << bit_index);
+   bp->msk |= ((mask_t)1 << bit_idx);
 }
 
 int findBlock(csa* c, int idx)
 {
-   int block_index = idx / MSKLEN;
-   unsigned int offset_key = block_index * MSKLEN;
+   int offset_unit = idx / MSKLEN;
+   unsigned int offset_key = offset_unit * MSKLEN;
    for (int i = 0; i < c->n; i++){
       if (c->b[i].offset == offset_key) {
          return i;
@@ -243,10 +258,10 @@ int findBlock(csa* c, int idx)
    return BLOCK_NOT_FOUND;
 }
 
-int findPosition(block *bp, int bit_index)
+int findPosition(block *bp, int bit_idx)
 {
    int pos = 0; // == how many occpuied before pos
-   for (int i = 0; i < bit_index; i++) {
+   for (int i = 0; i < bit_idx; i++) {
       if (bp->msk & ((mask_t)1 << i)) {
          pos++;
       }
@@ -257,52 +272,89 @@ int findPosition(block *bp, int bit_index)
 void move_and_insert(csa *c, unsigned int offset)
 {
    // noted: n has been updated
-   block temp = c->b[c->n-1];
+   int last_idx = c->n-ZERO_INDEX_OFFSET;
+   block temp = c->b[last_idx];
    int pos = 0;
-   for (int i = 0; i < c->n-1; i++){
+   for (int i = 0; i < last_idx; i++){
       if (c->b[i].offset < offset) {
          pos++;
       }
    }
-   for (int i = c->n-1; i > pos; i--) {
+   for (int i = last_idx; i > pos; i--) {
       c->b[i] = c->b[i-1];
    }
    c->b[pos] = temp;
 }
 
-bool delete(csa *c, int bp_index, int bit_index, int pos)
+bool delete(csa *c, int bp_idx, int bit_idx, int pos)
 {
    if (c == NULL) {
       return false;
    }
 
-   block *bp = &c->b[bp_index];
-   if (bp == NULL) {
-      return false;
-   }
-
-   bool isExist = bp->msk & ((mask_t)1 << bit_index);
-   if (!isExist) {
+   block *bp = &c->b[bp_idx];
+   // check if val exists
+   if (!isValExist(bp->msk, bit_idx)) {
       return false;
    }
    // 1. free block val
    // 1.a set to 0
    int counts = findPosition(bp, MSKLEN);
    // 1.b move one index to the left
-   for (int i = pos; i < counts-1 ; i++) {
+   for (int i = pos; i < counts-ZERO_INDEX_OFFSET ; i++) {
       bp->vals[i] = bp->vals[i+1];
    }
    // 1.c realloc 1 index
-   int new_len = counts - 1;
+   int new_len = counts-DESIRED_RESIZE;
    bp->vals = realloc(bp->vals, sizeof(int) * (new_len));
-
-   // 2. update mask
-   bp->msk &= ~((mask_t)1 << bit_index);
+   bp->msk &= ~((mask_t)1 << bit_idx);
 
    // 3. if whole block is empty, free it from csa ?
+   additional_free(c, bp_idx, new_len);
+   return true;
+}
+
+void concat_data(csa *c, char* s)
+{
+   char tmp[BIGSTR];
+   for (int i = 0; i < c->n; i++) {
+      
+      // open '{'
+      strcat(s, "{");
+   
+      int count = findPosition(&c->b[i], MSKLEN);
+      snprintf(tmp, BIGSTR , "%d|", count);
+      strcat(s, tmp);
+      
+      int val_idx = 0;
+      for (int bit_idx = 0; bit_idx < MSKLEN; bit_idx++) {
+         if (isValExist(c->b[i].msk, bit_idx)) {
+            int full_idx = c->b[i].offset + bit_idx;
+            int val = c->b[i].vals[val_idx];
+            snprintf(tmp, BIGSTR, "[%d]=%d", full_idx, val);
+            strcat(s, tmp);
+            if (val_idx != count - 1){
+               strcat(s, ":");
+            }
+            val_idx++;
+         }
+      }
+
+      // close '}'
+      strcat(s, "}");
+   }
+}
+
+bool isValExist(mask_t msk, int bit_idx)
+{
+   return (msk & ((mask_t)1 << bit_idx)) != NONE_OVERLAP;
+}
+
+void additional_free(csa *c, int bp_idx, int new_len)
+{
    if (new_len == 0){
       // 3.a move the remaining blocks to the left
-      for (int i = bp_index; i < c->n-1; i++) {
+      for (int i = bp_idx; i < c->n-1; i++) {
          c->b[i] = c->b[i+1];
       }
       c->n--;
@@ -314,10 +366,4 @@ bool delete(csa *c, int bp_index, int bit_index, int pos)
       free(c->b);
       c->b = NULL;
    }
-   return true;
 }
-
-// TODO?
-// (free)(realloc)(move)(insert)
-// csa increment/decrement -> arr b, n 
-// block increment/decrement -> vals, msk
--- CSA/mydefs.h
+++ CSA/mydefs.h
@@ -3,11 +3,17 @@
 
 #define BIGSTR 100000
 #define BLOCK_NOT_FOUND -1
+#define ZERO_INDEX_OFFSET 1
+#define DESIRED_RESIZE 1
+#define NONE_OVERLAP 0
 
 // Prototypes for other "private" functions etc.
 int findBlock(csa* c, int idx);
 int findPosition(block *bp, int bit_index);
 bool delete(csa *c, int bp_index, int bit_index, int pos);
+bool isValExist(mask_t msk, int bit_idx);
+void additional_free(csa *c, int bp_idx, int new_len);
 void incrementBlock(csa* c, int idx);
 void update_block(csa* c, int idx, int val);
 void move_and_insert(csa *c, unsigned int offset);
+void concat_data(csa *c, char* s);
